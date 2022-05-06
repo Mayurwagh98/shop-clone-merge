3b6e1284d8e5ab2baee645ef827bcc9452f9fd84
Mayur Wagh <69896733+Mayurwagh98@users.noreply.github.com>
2022-05-06 17:37:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+// import logo from './logo.svg'
+import './App.css'
+import {Shipping} from "./components/Shipping"
+import {Navbar} from "./components/Navbar/Navbar"
+
+
 
 function App() {
+  // const [count, setCount] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Shipping></Shipping>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
